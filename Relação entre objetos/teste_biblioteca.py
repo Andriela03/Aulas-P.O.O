dbf10af34f80730e62122bf1abc6d04584b8a2c4
Andriela Gleice <andriela.silva@escolar.ifrn.edu.br>
2024-09-03 22:36:05 -0300

    Alterações 03/09/24

--- Relação entre objetos/teste_biblioteca.py
+++ Relação entre objetos/teste_biblioteca.py
@@ -5,4 +5,11 @@ from biblioteca import Biblioteca
 def novo_livro():
     titulo = str(input('Digite o título do livro: '))
     nome = str(input('Digite o nome do autor: '))
-    nacionalidade = 
+    nacionalidade = str(input('Digite a nacionalidade do livro: '))
+    anoPublicacao = int(input('Digite o ano que o livro foi publicado: '))
+    
+    return Livro(titulo, nome, nacionalidade, anoPublicacao)
+
+    
+    
+    
